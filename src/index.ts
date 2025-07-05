0a92aae79703f78734f8bc769516c44d6558675b
yohanesokta <yohanesoktayt72@gmail.com>
2025-07-05 22:58:50 +0700

    fix : environtment2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config()
+    require('dotenv').config();
+    console.log("Load Dev Environtments");
 }
 import { Router as AuthController } from "./auth/auth.controller";
 import { adminRouter, adminUserRouter } from "./admin/admin.controller";
@@ -17,7 +18,6 @@ import {logger} from "./utils/prisma";
 import {createProductRouter } from "./admin/product/product.controller";
 import { midtramsRouter } from "./default/midtrans/midtrans.controller";
 import { PaymentController } from "./user/payment/payment.controller";
-import { configDotenv } from "dotenv";
 
 const app = express();
 const app_port = process.env.APP_PORT || 3000
